4c7935eb849dc45261dca163dea38fe022d2f32d
felixdub <felixdub@cim.mcgill.ca>
2016-10-06 15:17:02 -0400

    Add header and question

--- dns-client/src/DnsClient.java
+++ dns-client/src/DnsClient.java
@@ -9,6 +9,7 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class DnsClient {
 	
@@ -18,6 +19,10 @@ public class DnsClient {
 	static Integer timeout = 5;
 	static Integer maxRetries = 3;
 	static Integer port = 53;
+	
+	static int sendDataIndex = -1;
+	static byte[] sendData = new byte[1024];
+	static byte[] receiveData = new byte[1024];
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -65,23 +70,138 @@ public class DnsClient {
 			}
 		}
 		
-		//TODO: create the data to be send according to the DNS request specification
 		
-		//HEADER
-		byte[] ID = new byte[2];	//random number
-		byte[] H2 = new byte[2];
+		/*************************/
+		/********** HEADER *******/
+		/*************************/
+		/*
+		 * 	Header 1
+		 * 	ID			identifier						random	16bit
+		 */
+		byte[] ID = new byte[2];	//random
+		Random r = new Random();
+		r.nextBytes(ID);
 		
-		byte[] QDCOUNT = new byte[2];
-		QDCOUNT[0] = (byte) 0;
-		QDCOUNT[1] = (byte) 1;
+		sendData[sendDataIndex++] = ID[0];
+		sendData[sendDataIndex++] = ID[1];
 		
-		byte[] ANCOUNT = new byte[2];
 		
-		byte[] NSCOUNT = new byte[2];
+		/*	Header 2
+		 * 	QR			query (0) or response (1)?		0		1bit
+		 * 	Opcode		kind of query? Standard (0)		0		1bit
+		 * 	AA			authoritative response?			0		4bit
+		 * 	TC			truncated?						0		1bit
+		 * 	RD			recursion?						1		1bit
+		 * 	RA			server support recursion?		0		1bit
+		 * 	Z			for future use...				0		3bit
+		 * 	RCODE		error at the server?			0		4bit
+		 */
+		byte[] H2 = {(byte) 1, 0};
 		
-		byte[] ARCOUNT = new byte[2];
+		sendData[sendDataIndex++] = H2[0];
+		sendData[sendDataIndex++] = H2[1];
 		
-		//DNS QUESTION
+		/*
+		 * 	Header 3
+		 * 	QDCOUNT		number of entries				1		16bit
+		 */
+		byte[] QDCOUNT = {0, (byte) 1};
+		
+		sendData[sendDataIndex++] = QDCOUNT[0];
+		sendData[sendDataIndex++] = QDCOUNT[1];
+		
+		/*
+		 * 	Header 4
+		 * 	ANCOUNT		# resource records in answer	0		16bit
+		 */
+		byte[] ANCOUNT = {0,0};
+		
+		sendData[sendDataIndex++] = ANCOUNT[0];
+		sendData[sendDataIndex++] = ANCOUNT[1];
+		
+		/*
+		 * 	Header 5
+		 * 	NSCOUNT		# name server resource records	0		16bit
+		 */
+		byte[] NSCOUNT = {0,0};
+		
+		sendData[sendDataIndex++] = NSCOUNT[0];
+		sendData[sendDataIndex++] = NSCOUNT[1];
+		
+		/*
+		 * 	Header 6
+		 * 	ARCOUNT		# resource Additional records	0		16bit
+		 */
+		byte[] ARCOUNT = {0,0};
+		
+		sendData[sendDataIndex++] = ARCOUNT[0];
+		sendData[sendDataIndex++] = ARCOUNT[1];
+		
+		
+		
+		
+		
+		/**************************/
+		/******** QUESTION ********/
+		/**************************/
+		
+		
+		/*
+		 * 	Question 1
+		 * 	QNAME		domain name 				see below	N*16bit
+		 */
+		for(String s: labels){
+			int labelLength = s.length();
+			
+			sendData[sendDataIndex++] = (byte) labelLength;
+			
+			for(int i = 0; i< labelLength; i++){
+				sendData[sendDataIndex++] = (byte) s.charAt(i);
+			}
+		}
+		
+		//TODO
+		
+		/*
+		 * 	Question 2
+		 * 	QTYPE		query type					see below	16bit
+		 * 
+		 * 	0x0001		type-A
+		 * 	0x0002		type-NS
+		 * 	0x000f		type-MX
+		 */
+		byte[] QTYPE = new byte[2];
+		switch(recordType){
+			case "A" :
+				QTYPE[0] = 0;
+				QTYPE[1] = (byte) 1;
+				break;
+			
+			case "NS" :
+				QTYPE[0] = 0;
+				QTYPE[1] = (byte) 2;
+				break;
+			
+			case "MX" :
+				QTYPE[0] = 0;
+				QTYPE[1] = (byte) 15;
+				break;
+		}
+		
+		sendData[sendDataIndex++] = QTYPE[0];
+		sendData[sendDataIndex++] = QTYPE[1];
+		
+		/*
+		 *	Question 3
+		 *	QCLASS 		class of the query				1		16bit
+		 *	
+		 *	0x0001 means Internet address
+		 */
+		byte[] QCLASS = {0, (byte) 1};
+		
+		sendData[sendDataIndex++] = QCLASS[0];
+		sendData[sendDataIndex++] = QCLASS[1];
+		
 		
 		
 		
@@ -89,9 +209,6 @@ public class DnsClient {
 		DatagramSocket clientSocket = new DatagramSocket();
 		
 		InetAddress IPAddress = InetAddress.getByAddress(server);
-		
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
 		
 		//create datagram
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
